34418183d6e5cef589aef75f263a8ec4604fb964
Arnelle Balane <arnellebalane@gmail.com>
2019-01-25 14:01:15 +0800

    Add tests for .any() method

---
+++ methods/any/tests.js
@@ -0,0 +1,35 @@
+import test from 'ava';
+import any from '.';
+
+test('no predicate given, return false if array is empty', t => {
+    t.false(any([]));
+});
+
+test('no predicate given, return false if all values are falsey', t => {
+    t.false(any([0, false, null, undefined]));
+});
+
+test('no predicate given, return true if a value is truthy', t => {
+    t.true(any([0, false, null, undefined, true]));
+});
+
+test('predicate given, return false if array is empty', t => {
+    t.false(any(
+        [],
+        item => Boolean(item)
+    ));
+});
+
+test('predicate given, return false if predicate always returns false', t => {
+    t.false(any(
+        [0, false, null, undefined],
+        item => Boolean(item)
+    ));
+});
+
+test('predicate given, return true if predicate returns true', t => {
+    t.true(any(
+        [0, false, null, undefined, true],
+        item => Boolean(item)
+    ));
+});
