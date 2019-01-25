4c43dfd27852c9d450c6843dfbb0b67874553b83
Arnelle Balane <arnellebalane@gmail.com>
2019-01-26 02:22:50 +0800

    Add esdoc comments for .at() method

--- methods/at/index.js
+++ methods/at/index.js
@@ -1,3 +1,21 @@
+/**
+ *  @desc Returns the element at the given index. A negative index counts
+ *  from the end of the array. Returns `null` if the index is out of range.
+ *
+ *  @param {Array} array
+ *  @param {number} index - The index of the item to get from the array. A
+ *      negative value counts from the end of the array.
+ *
+ *  @return {*} The array item at the given index.
+ *
+ *  @example
+ *  at([1, 2, 3], 1); // 2
+ *  at([1, 2, 3], -1); // 3
+ *
+ *  @example
+ *  citrine([1, 2, 3]).at(1); // 2
+ *  citrine([1, 2, 3]).at(-1); // 3
+ */
 export default function at(array, index) {
     if (index < 0) {
         index += array.length;
