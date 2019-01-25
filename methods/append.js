8c21706e525b7866dcf9805bec362077f21d33f4
Arnelle Balane <arnellebalane@gmail.com>
2019-01-26 01:19:02 +0800

    Add definition for .append() method

---
+++ methods/append.js
@@ -0,0 +1,19 @@
+/**
+ *  @desc Appends the given object(s) on to the end of the given array.
+ *
+ *  @param {Array} array
+ *  @param {...*} args - The items to append to the array
+ *
+ *  @return {Array} The array with the new items appended to the end
+ *
+ *  @example
+ *  append([1, 2], 3); // => [1, 2, 3]
+ *  append([1, 2], 3, 4, 5); // => [1, 2, 3, 4, 5]
+ *
+ *  @example
+ *  citrine([1, 2]).append(3); // => [1, 2, 3]
+ *  citrine([1, 2]).append(3, 4, 5); // => [1, 2, 3, 4, 5]
+ */
+export default function append(array, ...args) {
+    return array.concat(args);
+}
