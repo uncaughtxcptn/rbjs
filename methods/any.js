cafdb2506fd5805638eab9c8cad0fa11cf2da57b
Arnelle Balane <arnellebalane@gmail.com>
2019-01-25 21:14:34 +0800

    Update esdoc comments for .any() method

--- methods/any.js
+++ methods/any.js
@@ -1,15 +1,20 @@
 /**
  *  @desc Passes each element of the array to the given function, and checks if
  *  any of the items makes the given function return a truthy value, or if any
- *  of the items is a truthy value.
+ *  of the items is a truthy value.<br>If instead a pattern is given, the
+ *  method returns whether any string item of the array matches the pattern.
  *
  *  @param {Array} array
- *  @param {Function} [predicate] - This function will be called for each array
- *      item. If this function ever returns a truthy value, `any` returns
- *      `true`.<br>If not given, defaults to `item => item`.
+ *  @param {Function|RegExp} [predicateOrPattern] - This function will be
+ *      called for each array item. If this function ever returns a truthy
+ *      value, `any` returns `true`.
+ *      <br>If this is a RegExp, `any` will return `true` if any array item is
+ *      is a string that matches this pattern.
+ *      <br>If not given, defaults to `item => item`.
  *
- *  @return {boolean} Whether any of the items in the array is a truthy value
- *  or made the given function return a truthy value.
+ *  @return {boolean} Whether any of the items in the array is a truthy value,
+ *  or made the given function return a truthy value, or is a string that
+ *  matches the given pattern.
  *
  *  @example
  *  any([null, undefined, true], Boolean); // true
