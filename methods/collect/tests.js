c5813d3ae8c55eafffd793e81d6a80e4e2d119d5
Arnelle Balane <arnellebalane@gmail.com>
2019-01-26 10:19:32 +0800

    Add tests for .collect() method

---
+++ methods/collect/tests.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+import collect from '.';
+
+test('return values returned by the given function when called with each array item', t => {
+    t.deepEqual(
+        collect([1, 2, 3], x => x * 2),
+        [2, 4, 6]
+    );
+
+    t.deepEqual(
+        collect(['a', 'b', 'c'], x => `${x}!`),
+        ['a!', 'b!', 'c!']
+    );
+});
