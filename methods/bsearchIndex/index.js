4db347cee1d0a2648432df7bbf3dce32a63e0e9e
Arnelle Balane <arnellebalane@gmail.com>
2019-01-26 20:09:38 +0800

    Implement both find-minimum and find-any modes for .bsearchIndex()

--- methods/bsearchIndex/index.js
+++ methods/bsearchIndex/index.js
@@ -1,16 +1,31 @@
 export default function bsearchIndex(array, callback) {
-    const index = (function search(arr, cb, idx) {
+    const [i, j] = (function search(arr, cb, _i, _j) {
         if (arr.length === 0) {
-            return idx;
+            return [_i, _j];
         }
 
-        const i = Math.floor(arr.length / 2);
-        const result = callback(arr[i]);
+        const idx = Math.floor(arr.length / 2);
+        const result = cb(arr[idx]);
 
-        return result
-            ? search(arr.slice(0, i), cb, idx)
-            : search(arr.slice(i + 1), cb, idx + i + 1);
-    })(array, callback, 0);
+        if (result === true) {
+            // Find-minimum mode: traverse left subtree
+            return search(arr.slice(0, idx), cb, _i, _j);
+        } else if (result === false) {
+            // Find-minimum mode: traverse right subtree
+            return search(arr.slice(idx + 1), cb, _i + idx + 1, _j);
+        } else if (result < 0) {
+            // Find-any mode: traverse left subtree
+            return search(arr.slice(0, idx), cb, Math.min(_i, idx), idx);
+        } else if (result > 0) {
+            // Find-any mode: traverse right subtree
+            return search(arr.slice(idx + 1), cb, (_i += idx + 1), Math.max(_j, _i));
+        }
+        // Find-any mode: return the index of any item that makes the given
+        // callback return 0.
+        return [(_i += idx), _j];
+    })(array, callback, 0, -1);
 
-    return index === array.length ? null : index;
+    // Find-minimum mode: return null if i === array.length
+    // Find-any mode: return null if i === j
+    return i === array.length || i === j ? null : i;
 }
