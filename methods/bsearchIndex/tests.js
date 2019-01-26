5a9db73526c77c493b528328ff43c8314f3cb09f
Arnelle Balane <arnellebalane@gmail.com>
2019-01-26 16:30:05 +0800

    Add test cases for .bsearchIndex() find-any mode

--- methods/bsearchIndex/tests.js
+++ methods/bsearchIndex/tests.js
@@ -13,3 +13,27 @@ test('find-minimum mode: return null if no item makes the condition return true'
     t.is(bsearchIndex([1], x => x < 0), null);
     t.is(bsearchIndex([0, 4, 7, 10, 12], x => x >= 100), null);
 });
+
+test('find-any mode: return any item that makes the condition return 0', t => {
+    t.true([1, 2].includes(bsearchIndex(
+        [0, 4, 7, 10, 12],
+        x => 1 - Math.floor(x / 4)
+    )));
+
+    t.true([1].includes(bsearchIndex(
+        [1, 2, 3, 4, 5],
+        x => 2 - x
+    )));
+});
+
+test('find-any mode: return null if no item makes the condition return 0', t => {
+    t.is(bsearchIndex([1, 2, 3], () => 1), null);
+    t.is(bsearchIndex([1, 2, 3], () => -1), null);
+    t.is(
+        bsearchIndex(
+            [0, 4, 7, 10, 12],
+            x => 4 - Math.floor(x / 2)
+        ),
+        null
+    );
+});
