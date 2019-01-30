a8f2100717385b9a7d9cac54164876ebfa548057
Arnelle Balane <arnellebalane@gmail.com>
2019-01-30 15:50:02 +0800

    Update esdoc comments for .collect() method

--- methods/collect/index.js
+++ methods/collect/index.js
@@ -2,13 +2,15 @@
  *  Invokes the given function once for each element of the array. Returns an
  *  array containing the values returned by the given function.
  *
+ *  If the callback function is not given, this returns the original array.
+ *
  *  @param {Array} array
- *  @param {Function} callback - Will be called for each item of the array. The
- *      returned value of this function will be the corresponding item in the
- *      resulting array.
+ *  @param {Function} [callback] - Will be called for each item of the array.
+ *      The returned value of this function will be the corresponding item in
+ *      the resulting array.
  *
  *  @return {Array} A new array containing the returned values of the given
- *      function.
+ *      function, or the original array of the callback function is not given.
  *
  *  @example
  *  collect([1, 2, 3], x => x * 2); // => [2, 4, 6]
