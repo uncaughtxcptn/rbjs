57ca9944e9481d87ac3e2867d9cf626d5eee84f4
Arnelle Balane <arnellebalane@gmail.com>
2019-01-31 22:09:54 +0800

    Update .all() implementation to pass new tests

--- methods/all/index.js
+++ methods/all/index.js
@@ -1,20 +1,15 @@
 /**
  *  @desc Passes each element of the array to the given function, and checks if
  *  all of the items make the given function return a truthy value, or if all
- *  of the items are truthy values.<br>If instead a pattern is given, the
- *  method returns whether all string items of the array match the pattern.
+ *  of the items are truthy values.
  *
  *  @param {Array} array
- *  @param {Function|RegExp} [predicateOrPattern] - This function will be
- *      called for each array item. If this function always returned truthy
- *      values, `all` returns `true`.
- *      <br>If this is a RegExp, `all` will return `true` if all array items
- *      are strings that match this pattern.
- *      <br>If not given, defaults to `item => item`.
+ *  @param {Function} [predicate] - This function will be called for each array
+ *      item. If this function always returned truthy values, `all` returns
+ *      `true`. If not given, defaults to `item => item`.
  *
  *  @return {boolean} Whether all of the items in the array are truthy values,
- *  or made the given function always return truthy values, or are strings that
- *  match the given pattern.
+ *  or made the given function always return truthy values.
  *
  *  @example
  *  all([1, 'string', true], Boolean); // true
@@ -22,13 +17,10 @@
  *  @example
  *  rbjs([1, 'string', true]).all(Boolean); // true
  */
-export default function all(array, predicateOrPattern) {
-    let predicate = predicateOrPattern;
-
-    if (predicateOrPattern instanceof RegExp) {
-        predicate = item => typeof item === 'string'
-            && predicateOrPattern.test(item);
-    } else if (!predicateOrPattern) {
+export default function all(array, predicate) {
+    if (arguments.length > 2) {
+        throw new Error('Too many arguments provided.');
+    } else if (!predicate) {
         predicate = item => item;
     }
 
