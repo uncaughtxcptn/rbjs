fb90a6feafec980f1dac063d55124f71ef5500c4
Arnelle Balane <arnellebalane@gmail.com>
2019-01-31 17:00:17 +0800

    Add .cycle() method to proxy handlers

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import collect from './methods/collect';
 import compact from './methods/compact';
 import concat from './methods/concat';
 import count from './methods/count';
+import cycle from './methods/cycle';
 
 const handlers = {
     all,
@@ -23,7 +24,8 @@ const handlers = {
     collect,
     compact,
     concat,
-    count
+    count,
+    cycle
 };
 
 export default function rbjs(toProxy) {
