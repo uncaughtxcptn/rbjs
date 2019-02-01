cbaed0a6eaf8bf70fb6185cba51b67fb28c355e6
aldnav <aldrinnavarro16@gmail.com>
2019-02-01 13:20:40 +0800

    Add concat method documentation

--- methods/concat/index.js
+++ methods/concat/index.js
@@ -1,4 +1,19 @@
-
+/**
+ * Appends the elements of `otherArrays` into the given array.
+ *
+ * @param {Array} array1
+ * @param {...any} otherArrays
+ *
+ * @return {Array} Returns the array itself.
+ *
+ * @example
+ * concat([1, 2, 3], [4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
+ * concat(['a', 'b'], ['c'], ['d', 'e']) // => ['a', 'b', 'c', 'd', 'e']
+ *
+ * @example
+ * rbjs([1, 2]).concat([3, 4]); // => [1, 2, 3, 4]
+ * rbjs(['a', 'b']).concat(['c', 'd'], ['e'])  // => ['a', 'b', 'c', 'd', 'e']
+ */
 export default function concat(array1, ...otherArrays) {
     otherArrays.forEach(arr => {
         array1.push(...arr);
