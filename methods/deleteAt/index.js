ae29da9d4efff2941def501fa5679de8ec0308e5
Arnelle Balane <arnellebalane@gmail.com>
2019-02-05 11:11:58 +0800

    Add esdoc comments for .deleteAt() method

--- methods/deleteAt/index.js
+++ methods/deleteAt/index.js
@@ -1,3 +1,22 @@
+/**
+ *  Deletes the element at the specified index, returning the removed element,
+ *  or `null` if the index is out of range.
+ *
+ *  @param {Array} array
+ *  @param {Number} index - The index to remove an element from. Can be a
+ *      negative value, in which case will start from the end of the array.
+ *
+ *  @return {*|null} The removed element, or `null` if the index is out
+ *      of bounds.
+ *
+ *  @example
+ *  deleteAt([1, 2, 3], 1); // => 2
+ *  deleteAt([1, 2, 3], -5); // => null
+ *
+ *  @example
+ *  rbjs([1, 2, 3]).deleteAt(1); // => 2
+ *  rbjs([1, 2, 3]).deleteAt(-5); // => null
+ */
 export default function deleteAt(array, index) {
     index = index < 0 ? array.length + index : index;
 
