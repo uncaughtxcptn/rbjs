1ee937b4a911af4ce9b0dcd12d99f01c45afd154
Arnelle Balane <arnellebalane@gmail.com>
2019-02-05 11:08:59 +0800

    Fix tests for .deleteAt() method

--- methods/deleteAt/tests.js
+++ methods/deleteAt/tests.js
@@ -2,10 +2,7 @@ import test from 'ava';
 import deleteAt from '.';
 
 test('removes the item at the specified index', t => {
-    t.deepEqual(
-        deleteAt([1, 2, 3, 4, 5], 2),
-        [1, 2, 4, 5]
-    );
+    t.is(deleteAt([1, 2, 3, 4, 5], 2), 3);
 });
 
 test('returns the removed element at the specified index', t => {
@@ -17,12 +14,6 @@ test('returns null and makes no modifications if the index is out of range', t =
 });
 
 test('accepts negative indeces', t => {
-    t.deepEqual(
-        deleteAt([1, 2, 3, 4, 5], -1),
-        [1, 2, 3, 4]
-    );
-    t.deepEqual(
-        deleteAt([1, 2, 3, 4, 5], -6),
-        [1, 2, 3, 4, 5]
-    );
+    t.is(deleteAt([1, 2, 3, 4, 5], -1), 5);
+    t.is(deleteAt([1, 2, 3, 4, 5], -6), null);
 });
