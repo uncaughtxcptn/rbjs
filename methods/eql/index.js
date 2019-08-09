554269df49b3a259dbf0c3b4f9feff5fc11677a1
Arnelle Balane <arnellebalane@gmail.com>
2019-08-09 21:59:05 +0800

    Add esdoc comments for .eql() method

--- methods/eql/index.js
+++ methods/eql/index.js
@@ -1,8 +1,26 @@
+/**
+ *  Returns whether the array and given array are the same object, or whether
+ *  they contain the same items.
+ *
+ *  @param {Array} array
+ *  @param {*} other - The object to be compared to the array.
+ *
+ *  @return {boolean} Whether the two arrays are the same object or have the
+ *      same contents.
+ *
+ *  @example
+ *  eql([1, 2, 3], [1, 2, 3]); // true
+ *
+ *  @example
+ *  rbjs([1, 2, 3]).eql([1, 2, 3]); // true
+ */
 export default function eql(array, other) {
-    if (array === other) {
-        return true;
-    } else if (!(other instanceof Array) || array.length !== other.length) {
+    if (!(array instanceof Array)
+    || !(other instanceof Array)
+    || array.length !== other.length) {
         return false;
+    } else if (array === other) {
+        return true;
     }
 
     for (let i = 0, l = array.length; i < l; i++) {
